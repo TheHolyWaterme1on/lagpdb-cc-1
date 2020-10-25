429f5779ef698deaaadf22258454bc5f96faa0da
Olde7325 <luca.berne@email.de>
2020-10-25 23:52:09 +0100

    update credits

--- Report-System/customReport.go
+++ Report-System/customReport.go
@@ -7,7 +7,7 @@
 
     Recommended Trigger type and trigger: Regex trigger with trigger `\A-r(eport)?u(ser)?(\s+|\z)`
 
-    Created by: Olde#7325 / BoomBoomer#4200
+    Created by: Olde#7325 / Idea by: BoomBoomer#4200
 */ }}
 
 {{ /*CONFIG AREA START*/ }}
