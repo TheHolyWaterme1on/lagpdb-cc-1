c5b7c85f7e0be45f5acadc8556ef8a2d6a6f4e34
Olde7325 <71897876+Olde7325@users.noreply.github.com>
2020-11-23 18:38:13 +0100

    del

--- Utility/Biography/Option 1- New overrides old/Option 1, Part 1- Command.go
+++
@@ -1,9 +0,0 @@
-{{$introChan := 671173998301937694}}
-{{$introMsg := 0}}
-{{if (dbGet .User.ID "intro-msg")}}
-{{$introMsg = toInt (dbGet .User.ID "intro-msg").Value}}
-{{end}}
-{{dbSet .User.ID "intro-msg" (toString .Message.ID)}} {{/* apparently integers get weird in db so store this as a string */}}
-{{if (getMessage $introChan $introMsg)}}
-     {{deleteMessage $introChan $introMsg}}
-{{end}}
--- Utility/Biography/Option 1- New overrides old/Option 2- Leave message.go
+++
@@ -1,2 +0,0 @@
-{{/*Delete intro on leave*/}}
-{{execCC <CCID of main> nil nil true}}
